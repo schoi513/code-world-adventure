eca4a56385a2dcbf869eaa80ef60d09d1714d81e
Samuel Choi <schoi@Samuels-MBP.attlocal.net>
2020-07-24 15:27:58 -0500

    final version with logos n testing done, from here on it will be going above and beyond

--- script.js
+++ script.js
@@ -5,123 +5,142 @@ const optionButtonsElement = document.getElementById('options');
 const textBanks = [
     {
         id: 1, 
-        text:'You got a streaming device as a gift from a friend. What will you do?',
+        text:'You wake up in the coding world, what do you do?',
         options: [
             {
-                text: 'Keep the device and stream away',
-                // setState: { device: true, cash: false},
+                text: 'Explore the code world',
                 nextText: 2
             },
             {
-                text:'Im not into it, return device for cash',
-                // setState: {device: false, cash: true},
-                nextText: 2
+                text:'Im scared... stay put where you are',
+                nextText: 3
             }
         ]
     },
     {
         id: 2,
-        text: 'Online coding has became a thing, will you jump ',
+        text: 'As you explore, you meet your GA instructors. They need your help with coding, what do you do? ',
         options: [
             {
-                text: 'Use the viewbot',
-                nextText: 3
+                text: 'Of course! help with the coding',
+                nextText: 4
                 
             },
             {
-                text: 'aaaaaa',
-                nextText:3
+                text: 'Im not so good at coding... I rather stay away',
+                nextText: 5
             }
 
         ]
     },
     {
         id: 3,
-        text: 'aaaaaaaa',
+        text: 'as you stay put wild css monster appears and take you away',
         options: [
             {
-                text: 'bbbbbbb',
-                nextText: 4
-            },
-            {
-                text: 'bbbbbb',
-                nextText: 4
-            },
-            {
-                text: 'bbbbbb',
-                nextText: 4
+                text: 'Game over',
+                nextText: -1,
+               
             }
         ]
     },
     {
         id: 4,
-        text: 'cccccc',
+        text: 'Instructors ask you build a code that will work as long as condition is met, which method do you use?',
         options: [
             {
-                text: 'cccccccccc',
-                nextText: 5
+                text: 'object method',
+                nextText: 6
             },
             {
-                text: 'ccccccc,',
-                nextText: 5
+                text: 'for loop,',
+                nextText: 7
             }
         ]
     },
     {
         id: 5,
-        text: 'ddddd',
+        text: 'as you stay away from your instructors, javascript assasin creeps behind you and drag you to endless callback function world',
         options:[
             {
-                text: 'eeeee',
-                nextText: 6
-            },
-            {
-                text: 'eeeeee',
-                nextText: 7
-            },
-            {
-                text: 'eeeeee',
-                nextText: 8,
+                text: 'Endless callback!!!, game over',
+                nextText: -1
             }
+           
         ]  
     },
     {
         id: 6,
-        text: 'fffffffffff',
+        text: 'That is not correct and you become prisoned in object [ ] box forever',
         options: [
             {
-                text: 'gggggg',
+                text: 'Boxed, game over',
                 nextText: -1
             }
         ]
     },
     {
         id: 7,
-        text: 'eeeeee',
+        text: 'A wild css monster appears, you see its center div container is off the alignment. what do you do?',
         options: [
             {
-                text: 'ggggg',
-                nextText: -1
+                text: 'set its height to auto',
+                nextText: 8
+
+            },
+            {
+                text: 'set align-item to center',
+                nextText: 9
             }
         ]
     },
     {
         id: 8,
-        text: 'eeeee',
+        text: 'That did nothing to monster and it got hold of you. Monster drag you to its flexbox prison',
         options: [
             {
-                text: 'ggggg',
+                text: 'Imprisoned in flexbox, game over',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id: 9,
+        text: 'the div container aligns and monster falls down. The instructors hand you 2 code balls. Which one do you use?',
+        options: [
+            {
+                text: ` "13" === "13" `,
+                nextText: 10
+            },
+            {
+                text: ` 13 === "13" `,
+                nextText: 11
+            }
+        ]
+    },
+    {
+        id: 10,
+        text: 'The code ball worked! Monster is captured and code world is safe',
+        options: [
+            {
+                text: 'You won!',
+            },
+        ]
+    },
+    {
+        id: 11,
+        text: 'That is incorrect, the code ball traps you into boolean realm',
+        options: [
+            {
+                text: 'trapped in boolean realm. Game over',
                 nextText: -1
             }
         ]
     }
 ]
 
-let state = {}
-
 function showOption(option){
     return option;
-    //option.requiredState === null || option.requiredState(state)
 }
 
 function selectOption(option) {
@@ -129,7 +148,6 @@ function selectOption(option) {
     if (nextTextBank <= 0){
         return start()
     }
-    state = Object.assign(state, option.setState)
     showText(nextTextBank)
 }
 
@@ -148,7 +166,7 @@ function showText (showTextIndex){
             button.innerText = option.text
             button.classList.add('clicks')
             button.addEventListener('click', () => selectOption(option))
-            console.log(option)
+            //console.log(option)
             optionButtonsElement.appendChild(button)
         }
     })
@@ -156,55 +174,7 @@ function showText (showTextIndex){
 
 function start(){
     showText(1)
-    state = {}
 }
 
 start();
 
-/* breaking it down to smallest pictures and scopes
-what kind of function or actions need?
-after game start and user read through welcome message
-1. function to be able to click each boxes
-    a. upon onclick events, that followers/cash/fatigue bar work accordingly
-2. if user does not have enough health or cash, the boxes that require certain number will not be
-able to use the action and show message "you do not have enough resources!"
-3. create set of random texts accordingly to the each event boxes and spit out the text to textbox
-when the action is pressed upon
-    
-4. store the random set boxes
-5. the dates will pass by per action boxes
-    a. have next day button setup to refresh fatigue bar
-6. the fatigue will decrease per action
-    a. refreshed upon next day button onclick
-7. save the current cash / follower number upon next day
-8. win condition function that if follower reached 1million, the browser stops and alert win message.
-
-breaking down action events
-aa. the losing percentage / lose amount will be small (going above and beyond)
-1. stream = gain small number of follower / gain money / fatigue bar goes down
-2. hosting event = can gain or *lose* bigger number of followers / lose money / fatigue bar goes down significantly
-3. streamathon = ++ gain of followers / bigger donations / larger loss of fatigue
-4. giveaway = possibly one of the biggest gain of follower / loss of significant money / almost no fatigue
-5 sponsor from big company = big gains on followers / earn significant money / almost no fatigue
-6. hosting offline event = biggest gain on follower / loss of money / entire fatigue wasted 
-
-breaking down random set of text boxes
-
-1. stream: [you have streamed couple people enjoyed your stream, new follower! 
-            you had a good day, some people started talk about you among community
-            you interacted with your fans really well, positivie day!]
-2. event [ hosting online event can be rought but community has heard you and lots of people dropped by
-            bigger number of audience than usual but you pulled through! 
-            you have created buzz around the community]
-3. streamathon [the streamathon is very tiring but its for a good cause and your reputation has increased
-                you got mentioned on local television about streamathon! community people took sharp notice
-                your followers have stayed with you overnight streaming, it is very special bond]
-4. giveaway [you have given away different sets of products that you use , it attracted lots of people
-            lots of new followers subscribed to win the giveway but they enjoyed your channel
-            you lost big money in purchasing items for the giveaway but it was succesful PR move!]
-
-5. sponsor from big company: []            
-
-going above and beyond
-** marked will be going above and beyond featues 
-    chance of losing or earning will be feature to look upon */
